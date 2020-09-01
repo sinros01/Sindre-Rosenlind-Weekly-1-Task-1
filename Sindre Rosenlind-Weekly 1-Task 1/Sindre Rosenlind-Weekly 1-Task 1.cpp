ff86e70be7063ba6363f5ec9537a4519b5d4f6e9
sinros01 <69473399+sinros01@users.noreply.github.com>
2020-09-01 18:54:50 +0200

    Add project files.

---
+++ Sindre Rosenlind-Weekly 1-Task 1/Sindre Rosenlind-Weekly 1-Task 1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+int age;
+long int phonenumber;
+std::string initial = "";
+std::string Fullname = "";
+std::string birthdate = "";
+
+int main() {
+	std::cout << "Enter your full name:\n";
+	std::getline(std::cin, Fullname);
+	std::cout << "Enter your age:\n";
+	std::cin >> age;
+	std::cout << "Enter your initial:\n";
+	char initial;
+	std::cin >> initial;
+	std::cout << "Enter your phone number:\n";
+	std::cin >> phonenumber;
+	std::cout << "Enter date of birth:\n"; 
+	std::getline(std::cin, birthdate);
+	std::cin.ignore();
+	std::cin, Fullname;
+	std::cin, birthdate;
+	std::cout << "Your name:         |" << Fullname << std::endl;
+	std::cout << "your age:          |" << age << std::endl;
+	std::cout << "your initial:      |" << initial << std::endl;
+	std::cout << "your phone number: |" << phonenumber << std::endl;
+	std::cout << "your date of birth:|" << birthdate << std::endl;
+
+
+}
+
